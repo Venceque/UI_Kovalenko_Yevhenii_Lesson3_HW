6c10a596e6d26b54a12fde35bc2b21a3d6b09dc2
vence <venceque@gmail.com>
2018-12-27 10:54:38 +0200

    Initial Commit

---
+++ UI_Kovalenko_Yevhenii_Lesson3_HW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UI_Kovalenko_Yevhenii_Lesson3_HW
+//
+//  Created by Evgeniy Vence on 27/12/2018.
+//  Copyright © 2018 Vence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
